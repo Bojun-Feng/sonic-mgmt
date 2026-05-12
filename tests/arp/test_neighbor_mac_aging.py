c1814dceee0741b6a8c154af8d6e46315a622ef6
Bojun Feng <bojundf@gmail.com>
2026-05-12 16:57:32 -0500

    [tests/arp]: Parameterize neighbor MAC aging test for IPv4 and IPv6
    The test hard-fails on IPv6-only topologies because it only discovers
    neighbors via show_ip_interface (IPv4). Since MAC/neighbor aging on
    link-down is protocol-agnostic, parameterize to cover both address
    families -- matching the pattern used by test_neighbor_mac_noptf.py.
    
    * Add ipVersion fixture parameterized with [4, 6] to test both address families
    * Add _find_neighbor() helper to discover peers per protocol version
    * Skip gracefully when no neighbor of the requested IP version exists
    
    Signed-off-by: Bojun Feng <bojundf@gmail.com>

--- tests/arp/test_neighbor_mac_aging.py
+++ tests/arp/test_neighbor_mac_aging.py
@@ -29,24 +29,69 @@ def shutdown_interface(duthosts, enum_rand_one_per_hwsku_frontend_hostname):
 
 
 class TestNeighborMacAging:
+    @pytest.fixture(params=[4, 6])
+    def ipVersion(self, request):
+        """
+            Parameterized fixture for IP versions. This Fixture will run the test twice for both
+            IPv4 and IPv6
+
+            Args:
+                request: pytest request object
+
+            Returns:
+                ipVersion (int): IP version to be used for testing
+        """
+        yield request.param
+
+    def _find_neighbor(self, asichost, ipVersion):
+        """
+            Find a neighbor IP and its interface for the given IP version.
+
+            Args:
+                asichost (SonicAsic): ASIC instance on the DUT
+                ipVersion (int): IP version (4 or 6)
+
+            Returns:
+                dut_intf (str): Interface name, or None if not found
+                neighbor_ip (str): Neighbor IP address, or None if not found
+        """
+        if ipVersion == 4:
+            ip_interfaces = asichost.show_ip_interface()["ansible_facts"]["ip_interfaces"]
+            logger.debug("ip_interfaces: " + str(ip_interfaces))
+            for intf in ip_interfaces.keys():
+                if "peer_ipv4" in ip_interfaces[intf] and ip_interfaces[intf]["peer_ipv4"] != "N/A":
+                    return intf, ip_interfaces[intf]["peer_ipv4"]
+        else:
+            ipv6_interfaces = asichost.show_ipv6_interface()["ansible_facts"]["ipv6_interfaces"]
+            logger.debug("ipv6_interfaces: " + str(ipv6_interfaces))
+            for intf in ipv6_interfaces.keys():
+                if "peer_ipv6" in ipv6_interfaces[intf] and ipv6_interfaces[intf]["peer_ipv6"] != "N/A":
+                    return intf, ipv6_interfaces[intf]["peer_ipv6"]
+
+        return None, None
+
     def testNeighborMacAgingAfterIntfDown(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname,
-                                          enum_rand_one_frontend_asic_index, shutdown_interface):
+                                          enum_rand_one_frontend_asic_index, shutdown_interface, ipVersion):
         """
             Test whether neighbor MAC is aged out after interface down
+
+            Args:
+                duthosts: DUT hosts fixture
+                enum_rand_one_per_hwsku_frontend_hostname: random frontend DUT
+                enum_rand_one_frontend_asic_index: random frontend ASIC
+                shutdown_interface: fixture to shutdown/restore interface
+                ipVersion (Fixture<int>): IP version to be tested
+
+            Returns:
+                None
         """
         duthost = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
         asichost = duthost.asic_instance(enum_rand_one_frontend_asic_index)
-        ip_interfaces = asichost.show_ip_interface()["ansible_facts"]['ip_interfaces']
-        logger.debug("ip_interfaces: " + str(ip_interfaces))
 
-        dut_intf = None
-        neighbor_ip = None
-        for intf in ip_interfaces.keys():
-            if "peer_ipv4" in ip_interfaces[intf] and ip_interfaces[intf]["peer_ipv4"] != "N/A":
-                dut_intf = intf
-                neighbor_ip = ip_interfaces[intf]["peer_ipv4"]
-                break
-        pytest_assert(dut_intf is not None, "No IP interface found on DUT")
+        dut_intf, neighbor_ip = self._find_neighbor(asichost, ipVersion)
+        if dut_intf is None:
+            pytest.skip("No IPv{} neighbor found on DUT".format(ipVersion))
+
         logger.debug("DUT interface: {}, neighbor IP: {}".format(dut_intf, neighbor_ip))
 
         # Verify that the MAC address is present in the ARP table and ASIC_DB
